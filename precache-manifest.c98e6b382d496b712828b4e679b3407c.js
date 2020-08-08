04b77c34682522744221bf74b9836f64ca0230a5
bluezhan <2252263040@qq.com>
2020-08-08 19:40:26 +0800

    create project

---
+++ precache-manifest.c98e6b382d496b712828b4e679b3407c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2d502d9a4b318d4431",
+    "url": "/css/app.f1a853a1.css"
+  },
+  {
+    "revision": "b7252cde93b54af29948d8473f1ef7ba",
+    "url": "/img/bg.b7252cde.jpg"
+  },
+  {
+    "revision": "63ef37a479f43e2feaa9f9e7d0a46611",
+    "url": "/img/overlay.63ef37a4.png"
+  },
+  {
+    "revision": "14fbfdaca933cea9c15d25e3ec2c7e98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c2d502d9a4b318d4431",
+    "url": "/js/app.4d2b23f2.js"
+  },
+  {
+    "revision": "74d1e043c4d054d7e3ae",
+    "url": "/js/chunk-vendors.94e0a2e8.js"
+  },
+  {
+    "revision": "ba5c570ab89a4e0a23e2fa466046f1d2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
